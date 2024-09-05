628f58e609ec26e0dae746920f6de558b961c2f8
殷迦南 <thlz998@gmail.com>
2024-09-05 21:06:02 +0800

    no message

---
+++ YiAiQuickDeploy/public/assets/mjDraw-17cf5f12.js
@@ -0,0 +1,1 @@
+import{b_ as e,b$ as a}from"./index-4944f01f.js";function u(r){return e({url:"/fanyi/translate",data:r})}function n(r){return a({url:"/queue/addMjDrawQueue",data:r})}function s(r){return e({url:"/midjourney/proxy",data:r,headers:{responseType:"arraybuffer"}})}export{u as a,s as b,n as f};
