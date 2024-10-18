8d881fff973056f42b517469b471a368d05b1d50
Fishkiller111 <137759258+Fishkiller111@users.noreply.github.com>
2024-10-18 18:08:55 +0800

    侧边栏更新完毕
    Signed-off-by: Fishkiller111 <137759258+Fishkiller111@users.noreply.github.com>

--- chat/src/layout/index.vue
+++ chat/src/layout/index.vue
@@ -1,127 +1,362 @@
-<script setup lang="ts">
-import { computed, onMounted } from 'vue'
-import { NLayoutContent, useMessage } from 'naive-ui'
+<script setup lang='ts'>
+import {
+  computed,
+  defineAsyncComponent,
+  nextTick,
+  onMounted,
+  ref,
+  shallowRef,
+  watch,
+} from 'vue'
+import { NAvatar, NIcon, NScrollbar, NTooltip, useMessage } from 'naive-ui'
+import { PersonAddOutline, PersonRemoveOutline } from '@vicons/ionicons5'
 import { useRoute, useRouter } from 'vue-router'
-import SiderBar from './siderBar/index.vue'
-import FooterBar from './footerBar/index.vue'
-import Login from './components/Login.vue'
-import PayDialog from './components/PayDialog.vue'
-import GoodsDialog from './components/GoodsDialog.vue'
-import NoticeDialog from './components/NoticeDialog.vue'
-import BindWxDialog from './components/BindWx.vue'
-import SignInDialog from './components/SignInDialog.vue'
-import ModelDialog from './components/modelDialog.vue'
-import { useBasicLayout } from '@/hooks/useBasicLayout'
-import { useAppStore, useAuthStore, useGlobalStoreWithOut } from '@/store'
-import { fetchLoginByCodeAPI, fetchWxLoginRedirectAPI } from '@/api/user'
-import Loading from '@/components/base/Loading.vue'
-import type { ResData } from '@/api/types'
+import Logo from './Logo.vue'
+import { HoverButton, SvgIcon } from '@/components/common'
+import defaultAvatar from '@/assets/avatar.png'
+import macTablebar from '@/components/base/macTablebar.vue'
+import { fetchQueryMenuAPI } from '@/api/config'
 
+import { useAppStore, useAuthStore, useGlobalStoreWithOut } from '@/store'
+import { useBasicLayout } from '@/hooks/useBasicLayout'
+const Setting = defineAsyncComponent(
+  () => import('@/components/common/Setting/index.vue'),
+)
+
+const appStore = useAppStore()
 const authStore = useAuthStore()
+const useGlobalStore = useGlobalStoreWithOut()
+const message = useMessage()
+const track = ref(null)
+appStore.setEnv()
+
+const avatar = computed(() => authStore.userInfo.avatar)
 const route = useRoute()
 const router = useRouter()
-const useGlobalStore = useGlobalStoreWithOut()
-const appStore = useAppStore()
-const ms = useMessage()
-const payDialog = computed(() => useGlobalStore.payDialog)
-const goodsDialog = computed(() => useGlobalStore.goodsDialog)
-const noticeDialog = computed(() => useGlobalStore.noticeDialog)
-const bindWxDialog = computed(() => useGlobalStore.bindWxDialog)
-const signInDialog = computed(() => useGlobalStore.signInDialog)
-const modelDialog = computed(() => useGlobalStore.modelDialog)
-const { isMobile } = useBasicLayout()
-const loginDialog = computed(() => authStore.loginDialog)
-const globalConfigLoading = computed(() => authStore.globalConfigLoading)
-const theme = computed(() => appStore.theme)
-const bgColor = computed(() => theme.value === 'dark' ? '#24272e' : '#fff')
+const show = ref(false)
 const isLogin = computed(() => authStore.isLogin)
-const wechatSilentLoginStatus = computed(() => Number(authStore.globalConfig?.wechatSilentLoginStatus) === 1)
-const homePath = computed(() => authStore.globalConfig?.clientHomePath)
+const darkMode = computed(() => appStore.theme === 'dark')
+const env = computed(() => appStore.env)
+const logInIcon = shallowRef(PersonAddOutline)
+const logOutIcon = shallowRef(PersonRemoveOutline)
 
-/* 如果在vx环境并且携带了code则静默登录 */
-
-function handleCheckOtherLoginByToken() {
-  const { code } = route.query
-  console.log('code==', code)
-  if (code && !isLogin.value)
-    authStore.code2token(code)
-    // authStore.setToken(code)
-    // const name = route.name
-    // router.replace({ name, query: {} })
-    // ms.success('账户登录成功、开始体验吧！')
-    // authStore.getUserInfo()
-
-}
-
-/* 微信环境静默登录 */
-async function loginByWechat() {
-  if (homePath.value || !wechatSilentLoginStatus.value)
+async function queryMenu() {
+  const res: any = await fetchQueryMenuAPI({ menuPlatform: 1 })
+  if (!res.success)
     return
-  if (isLogin.value)
-    return
-
-  /* 如果在vx环境并且携带了code则静默登录 */
-  const { code } = route.query
-
-  if (code) {
-    const res: ResData = await fetchLoginByCodeAPI({ code: code as string })
-    if (res.success) {
-      authStore.setToken(res.data)
-      authStore.getUserInfo()
-    }
-  }
-  else {
-    const url = window.location.href.replace(/#.*$/, '')
-    const res: ResData = await fetchWxLoginRedirectAPI({ url })
-    if (res.success)
-      window.location.href = res.data
-  }
+  menuList.value = res.data
+  nextTick(() => {
+    calcExceededTotalWidth()
+  })
 }
 
-function init() {
-  const ua = window.navigator.userAgent.toLowerCase()
-  if (ua.match(/MicroMessenger/i) && ua?.match(/MicroMessenger/i)?.[0] === 'micromessenger')
-    loginByWechat()
+interface MenuItem {
+  menuName: string
+  menuPath: string
+  menuIcon: string
+  menuTipText: string
+  menuIframeUrl: string
+  isJump: boolean
+  isNeedAuth: boolean
 }
+
+const menuList = ref<MenuItem[]>([])
+const isNeedScroll = ref(false)
 
 onMounted(() => {
-  init()
-  handleCheckOtherLoginByToken()
+  queryMenu()
 })
 
-const getMobileMainClass = computed(() => {
-  if (isMobile.value)
-    return ['rounded-none', 'shadow-none']
-  return ['dark:border-neutral-800']
+const signInStatus = computed(
+  () => Number(authStore.globalConfig?.signInStatus) === 1,
+)
+
+function toggleLogin() {
+  if (isLogin.value)
+    authStore.logOut()
+  else authStore.setLoginDialog(true)
+}
+
+function checkMode() {
+  const mode = darkMode.value ? 'light' : 'dark'
+  appStore.setTheme(mode)
+}
+
+function setting() {
+  if (!isLogin.value)
+    authStore.setLoginDialog(true)
+  else show.value = true
+}
+const { isMobile } = useBasicLayout()
+
+const activeRoutePath = computed(() => {
+  return route.path
+})
+
+function toPath(name: string) {
+  router.push({ name })
+}
+
+const mobileSafeArea = computed(() => {
+  if (isMobile.value) {
+    return {
+      paddingBottom: 'env(safe-area-inset-bottom)',
+    }
+  }
+  return {}
 })
 
 const getMobileLayoutClass = computed(() => {
   if (isMobile.value)
-    return ['flex-col']
-  return ['dark:border-neutral-800']
+    return ['flex-rol', 'w-full', 'border-0']
+  return ['flex-col', 'w-sider', 'h-full', 'border-r']
 })
+
+const getIconMobileLayoutClass = computed(() => {
+  if (isMobile.value)
+    return ['flex', 'flex-rol', 'items-center', 'pt-0', 'w-full']
+  return ['flex', 'flex-col', 'pt-1', 'items-center']
+})
+
+const iframeSrc = computed(() => useGlobalStore.iframeUrl)
+
+function handleClickMenu(menu: MenuItem) {
+  const { menuPath, isJump, menuIframeUrl, isNeedAuth } = menu
+  if (isNeedAuth && !isLogin.value) {
+    message.warning('请先登录后访问！')
+    authStore.setLoginDialog(true)
+    return
+  }
+  useGlobalStore.updateIframeUrl('')
+  if (menuPath) {
+    return router.push({ path: menuPath })
+  }
+  else {
+    if (isJump) {
+      window.open(menuIframeUrl)
+    }
+    else {
+      useGlobalStore.updateIframeUrl(menuIframeUrl)
+      router.push({ path: '/extend' })
+    }
+  }
+}
+
+function handleSignIn() {
+  if (!isLogin.value) {
+    authStore.setLoginDialog(true)
+    return
+  }
+  useGlobalStore.updateSignInDialog(true)
+}
+
+function calcExceededTotalWidth() {
+  if (!track.value)
+    return
+  const { clientHeight = 0, scrollHeight = 0 } = track.value
+  isNeedScroll.value = scrollHeight > clientHeight
+}
+
+function isActive(item: MenuItem) {
+  const { menuIframeUrl, menuPath } = item
+  if (menuIframeUrl)
+    return menuIframeUrl === iframeSrc.value
+
+  if (menuPath)
+    return menuPath === activeRoutePath.value
+}
+
+watch(
+  isMobile,
+  (val) => {
+    appStore.setSiderCollapsed(val)
+  },
+  {
+    immediate: true,
+    flush: 'post',
+  },
+)
 </script>
 
 <template>
-  <div class="h-full dark:bg-[#24272e] transition-all p-0">
-    <div class="h-full overflow-hidden">
-      <div class="z-40 transition flex h-full relative" :class="getMobileLayoutClass">
-        <SiderBar v-if="!isMobile" />
-        <NLayoutContent class="h-full" style="flex: 1" :class="getMobileMainClass">
-          <RouterView v-slot="{ Component, route }">
-            <component :is="Component" :key="route.fullPath" />
-          </RouterView>
-        </NLayoutContent>
-        <FooterBar v-if="isMobile" />
-        <Loading v-if="globalConfigLoading" :bg-color="bgColor" />
-      </div>
-      <Login :visible="loginDialog" />
-      <PayDialog :visible="payDialog" />
-      <GoodsDialog :visible="goodsDialog" />
-      <NoticeDialog :visible="noticeDialog" />
-      <BindWxDialog :visible="bindWxDialog" />
-      <SignInDialog :visible="signInDialog" />
-      <ModelDialog :visible="modelDialog" />
+  <div
+    class="flex min-w-[70px] bg-[#e8eaf1] pb-2 dark:bg-[#25272d] dark:border-[#3a3a40] border-#efeff5-800"
+    :class="getMobileLayoutClass"
+    :style="mobileSafeArea"
+  >
+    <macTablebar v-if="env === 'electron'" />
+    <div class="px-2 w-full ele-drag">
+      <Logo />
+    </div>
+    <main
+      ref="track"
+      class="flex-1 flex-grow-1 mb-5 overflow-auto"
+      :class="[getIconMobileLayoutClass]"
+    >
+      <NScrollbar :size="1">
+        <div class="flex h-full flex-col items-center space-y-5">
+          <div
+            v-for="item in menuList"
+            :key="item.menuName"
+            class="flex justify-center flex-col items-center button-spacing"
+            :class="isMobile ? 'mt-0' : 'mt-4'"
+            @click="handleClickMenu(item)"
+          >
+            <NTooltip
+              v-if="!isMobile"
+              trigger="hover"
+              placement="right"
+            >
+              <template #trigger>
+                <div
+                  class="h-10 w-10 cursor-pointer bg-white dark:bg-[#34373c] rounded-lg duration-300 flex justify-center items-center btnhover"
+                  :class="[
+                    isActive(item)
+                      ? 'borderRadis shadow-[#3076fd] btns'
+                      : 'border-transparent',
+                  ]"
+                >
+                  <div class="icon-wrapper">
+                    <SvgIcon
+                      :icon="item.menuIcon"
+                      class="text-2xl transition-all"
+                      :class="[
+                        isActive(item)
+                          ? 'text-[#0C0E0C] dark:text-[#3076fd]'
+                          : '',
+                      ]"
+                    />
+                  </div>
+                </div>
+              </template>
+              {{ item.menuTipText }}
+            </NTooltip>
+
+            <!-- 改动2：改左侧菜单 -->
+            <!-- <span
+              class="text-[12px] mt-1 margin-auto whitespace-nowrap overflow-hidden"
+              :class="[
+                isActive(item)
+                  ? 'text-[#3076fd] dark:text-[#86dfba] '
+                  : 'text-[#999999]',
+              ]"
+            >{{ item.menuTipText }}</span> -->
+          </div>
+        </div>
+      </NScrollbar>
+    </main>
+
+    <!-- <HoverButton tooltip="全局设置" :placement="isMobile ? 'bottom' : 'right'" :class="isMobile ? 'pb-0' : 'pb-1' " @click="setting">
+      <NIcon size="20" color="#555">
+        <SvgIcon class="text-2xl" icon="fluent:dark-theme-24-regular" />
+      </NIcon>
+    </HoverButton> -->
+    <!-- <HoverButton v-if="isLogin" tooltip="个人中心" :placement="isMobile ? 'bottom' : 'right'" :class="isMobile ? 'pb-0' : 'pb-8' " @click="toPath('UserCenter')"> -->
+    <!-- <SvgIcon icon="icon-park-twotone:personal-collection" /> -->
+    <!-- h-[140px] -->
+    <div class="flex flex-col justify-between items-center">
+      <NTooltip
+        v-if="!isMobile && signInStatus"
+        trigger="hover"
+        placement="right"
+      >
+        <template #trigger>
+          <SvgIcon
+            class="text-xl cursor-pointer mb-5"
+            icon="streamline-emojis:wrapped-gift-1"
+            style="color: red"
+            @click="handleSignIn"
+          />
+        </template>
+        签到奖励
+      </NTooltip>
+
+      <!-- NTooltip v-if="!isMobile" trigger="hover" placement="right">
+        <template #trigger>
+          <SvgIcon
+            class="text-xl cursor-pointer mb-5"
+            :icon="darkMode ? 'noto-v1:last-quarter-moon-face' : 'twemoji:sun'"
+            @click="checkMode"
+          />
+        </template>
+        主题切换
+      </NTooltip -->
+
+      <NTooltip v-if="isLogin" trigger="hover" placement="right">
+        <template #trigger>
+          <NAvatar
+            :size="42"
+            :src="avatar"
+            round
+            bordered
+            :fallback-src="defaultAvatar"
+            class="cursor-pointer"
+            @click="toPath('UserCenter')"
+          />
+        </template>
+        个人中心
+      </NTooltip>
+
+      <HoverButton
+        v-if="!isLogin"
+        tooltip="登录账户"
+        :placement="isMobile ? 'bottom' : 'right'"
+        :class="isMobile ? 'mb-0' : 'mb-5'"
+        @click="toggleLogin"
+      >
+        <NIcon size="20" color="#555">
+          <component :is="logInIcon" />
+        </NIcon>
+      </HoverButton>
     </div>
   </div>
+  <Setting v-if="show" v-model:visible="show" />
 </template>
+
+<style>
+.sidebar {
+  overflow: hidden;
+  width: calc(100% - 5px);
+}
+.sidebar:hover {
+  width: 100%;
+  overflow: overlay;
+}
+
+.overlay {
+  overflow: hidden;
+}
+.overlay:hover {
+  width: 100%;
+  overflow: overlay;
+}
+
+.active_bar {
+  border-width: 1px;
+}
+
+.btns {
+  box-shadow: 0 5px 16px #000000;
+}
+.btnhover:hover{
+    box-shadow: 0 5px 16px #000000;
+    transform: scale(1.1);
+    color: #000000;
+}
+
+.borderRadis {
+  border-radius: 0.75rem;
+}
+.icon-wrapper:hover {
+  background-color: #27E093; /* 鼠标滑过时背景变色 */
+  padding: 12px; /* 增加内边距，让下方视觉上变粗 */
+  filter: drop-shadow(0px 4px 0px rgba(0, 0, 0, 1)); /* 添加不透明的黑色阴影 */
+}
+.icon-wrapper {
+  display: inline-block; /* 使外层容器适应内容 */
+  border: 2.5px solid black; /* 黑色边框，保持宽度 */
+  border-radius: 14px; /* 圆角 */
+  padding: 8px; /* 增加内边距来扩大外边框的大小 */
+  transition: background-color 0.3s, padding 0.3s; /* 添加过渡效果 */
+}
+</style>
