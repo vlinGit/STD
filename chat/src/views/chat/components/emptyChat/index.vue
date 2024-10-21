70054bf67847ba0d0843be07ba91ec2cb54c0dbf
Fishkiller111 <137759258+Fishkiller111@users.noreply.github.com>
2024-10-21 14:50:32 +0800

    对话开始页升级
    Signed-off-by: Fishkiller111 <137759258+Fishkiller111@users.noreply.github.com>

--- chat/src/views/chat/components/emptyChat/index.vue
+++ chat/src/views/chat/components/emptyChat/index.vue
@@ -1,32 +1,54 @@
 <script setup lang="ts">
-import {ref} from 'vue'
-import { SvgIcon } from '@/components/common'
+import { ref } from 'vue'
 import { NButton } from 'naive-ui'
+import { SvgIcon } from '@/components/common'
 import { useChatStore } from '@/store'
 
 const chatStore = useChatStore()
 
 const loading = ref(false)
 
-async function handleAddNewGroup(){
-	loading.value = true
-	try {
-		await chatStore.addNewChatGroup()
-		await chatStore.queryMyGroup()
-		loading.value = false
-	} catch (error) {
-		loading.value = false
-	}
+async function handleAddNewGroup() {
+  loading.value = true
+  try {
+    await chatStore.addNewChatGroup()
+    await chatStore.queryMyGroup()
+    loading.value = false
+  }
+  catch (error) {
+    loading.value = false
+  }
 }
-
-
 </script>
 
 <template>
-	<div class=" w-full h-full flex justify-center items-center flex-col">
-			<SvgIcon icon="tabler:message" class="mb-4 inline-block text-6xl text-[#2080f0]" />
-			<h1 class="mb-4 text-2xl font-bold mt-1">对话聊天</h1>
-			<p class="mb-6 text-base text-slate-500">点击下方按钮，开始一个新的对话吧</p>
-		 <NButton type="primary" :loading="loading" @click="handleAddNewGroup">新对话</NButton>
-	</div>
+  <div class=" w-full h-full flex justify-center items-center flex-col">
+    <SvgIcon icon="token:chat" class="mb-4 inline-block text-6xl" style="color: #27E093;font-size: 6rem;" />
+    <h1 class="mb-4 text-2xl font-bold mt-1">
+      开始使用Pockyt AI
+    </h1>
+    <p class="mb-6 text-base text-slate-500">
+      点击下方按钮，免费开启试用
+    </p>
+    <NButton class="button" :loading="loading" @click="handleAddNewGroup" />
+  </div>
 </template>
+
+<style scoped>
+.button {
+  background-color: #ffffff; /* 设置按钮背景颜色 */
+  border: 2px solid #000000; /* 设置边框颜色 */
+  border-radius: 40px; /* 设置圆角 */
+  width: 100px;
+  height: 50px;
+}
+.button:hover {
+  background-color: #27E093; /* 设置按钮悬停背景颜色 */
+  border-bottom: 6px solid #000000; /* 添加加粗的底边 */
+}
+.button:after {
+  font-size: 16px; /* 设置字体大小 */
+  font-weight: bold; /* 设置文本为加粗 */
+  content: "新对话"!important; /* 设置按钮文字 */
+}
+</style>
